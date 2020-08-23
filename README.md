01129b4dd19800aaaa15ab2386cca4f5de186832
uniquexiaobai <uniquexiaobai@gmail.com>
2020-08-23 15:34:39 +0800

    feat: support create instance

--- README.md
+++ README.md
@@ -18,7 +18,20 @@ tinyStorage.get('a', 1);
 tinyStorage.set('b', { x: 2 });
 tinyStorage.set('c', 3, { maxAge: 60 }); // expires after 1 minute
 
-tinyStorage.get('a'); // output: 1
+tinyStorage.get('b'); // output: { x: 2}
 
-tinyStorage.remove('a');
+tinyStorage.remove('a'); // remove a item
+
+tinyStorage.size(); // output: 2
+
+tinyStorage.clear(); // remove all items
+
+tinyStorage.size(); // output: 0
+
+// create a new instance, different instance don't affect each other
+const instance = tinyStorage.create({
+  name: 'mystorage', // prefix for the key
+  driver: sessionStorage, // default: localStorage
+  maxAge: 60, // set the default maxAge
+});
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,126 @@
-const get = (key: string) => {
+type Options = {
+  name?: string;
+  driver?: any;
+  maxAge?: number;
+};
+
+const getKeys = (baseOptions: Options) => (): string[] => {
+  const { name, driver } = baseOptions;
+
+  let i = 0,
+    len = driver.length,
+    keys = [],
+    key;
+  for (; i < len; i++) {
+    key = driver.key(i);
+    if (key.startsWith(name)) {
+      keys.push(key);
+    }
+  }
+
+  return keys;
+};
+
+const get = (baseOptions: Options) => (key: string) => {
+  const { name, driver } = baseOptions;
+
   const now = Date.now();
   let store;
 
   try {
-    store = JSON.parse(localStorage.getItem(key) as string);
+    store = JSON.parse(driver.getItem(`${name}__${key}`) as string);
   } catch (err) {
     return null;
   }
 
   if (!store || store.data == null) return null;
 
-  if (store.expires > 0 && store.expires <= now) return null;
+  if (store.expires && store.expires <= now) return null;
 
   return store.data;
 };
 
-type Options = {
-  maxAge: number;
-};
+const set = (baseOptions: Options) => (
+  key: string,
+  value: any,
+  options: Options
+) => {
+  const { name, driver, maxAge = 0 } = { ...baseOptions, ...options };
 
-const set = (key: string, value: any, options: Options = { maxAge: 0 }) => {
   const now = Date.now();
-  const expires = options.maxAge > 0 ? now + options.maxAge * 1000 : 0;
+  const expires = maxAge > 0 ? now + maxAge * 1000 : void 0;
   const store = JSON.stringify({
     data: value,
     expires,
   });
 
   try {
-    localStorage.setItem(key, store);
+    driver.setItem(`${name}__${key}`, store);
     return true;
   } catch (err) {
     return false;
   }
 };
 
-const remove = (key: string) => {
-  localStorage.removeItem(key);
+const remove = (baseOptions: Options) => (key: string) => {
+  const { name, driver } = baseOptions;
+
+  driver.removeItem(`${name}__${key}`);
 };
 
-const clear = () => {
-  localStorage.clear();
+const clear = (baseOptions: Options) => () => {
+  const { name, driver } = baseOptions;
+
+  if (!name) {
+    driver.clear();
+  } else {
+    const keys = getKeys(baseOptions)();
+    keys.forEach(key => {
+      driver.removeItem(key);
+    });
+  }
 };
 
-const size = () => {
-  return localStorage.length;
+const size = (baseOptions: Options) => () => {
+  const { name, driver } = baseOptions;
+
+  if (!name) return driver.length;
+
+  const keys = getKeys(baseOptions)();
+
+  return keys.length;
 };
 
-// TODO create instance
-function create() {}
+// name: 储存的 key 的前缀
+// driver: localStorage/sessionStorage
+// maxAge: 默认过期时间，单位：秒
+function create(options: Options) {
+  options.name = options.name || '';
+  options.maxAge = options.maxAge || 0;
+  options.driver = options.driver || localStorage;
+
+  return {
+    get: get(options),
+    set: set(options),
+    remove: remove(options),
+    clear: clear(options),
+    size: size(options),
+    create: create,
+  };
+}
+
+const defaultOptions: Options = {
+  name: '',
+  maxAge: 0,
+  driver: localStorage,
+};
 
 const tinyStorage = {
-  get,
-  set,
-  remove,
-  clear,
-  size,
+  get: get(defaultOptions),
+  set: set(defaultOptions),
+  remove: remove(defaultOptions),
+  clear: clear(defaultOptions),
+  size: size(defaultOptions),
   create,
 };
 
